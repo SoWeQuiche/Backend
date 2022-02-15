aa890cad72ec899501db9bac53ddc8db6ba1faef
MaxenceMottard <maxence.mottard@worldline.com>
2022-02-15 13:23:31 +0100

    deploy

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
-if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require('dotenv').config();
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 export default {
   port: process.env.PORT,
