fd8fb69f60f984a1509f6eb8a75b81bd38fb0108
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2022-02-15 09:29:53 +0100

    feat: create apple-app-site-association route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,6 +8,20 @@ export class AppController {
   @Get()
   getInfos(): string {
     return this.appService.getInfos();
+  }
 
+  @Get('apple-app-site-association')
+  getAppleAppSiteAssociation() {
+    return {
+      applinks: {
+        apps: [],
+        details: [
+          {
+            appID: 'S4J5D3N22R.com.maxencemottard.swq',
+          },
+        ],
+      },
+      webcredentials: { apps: ['S4J5D3N22R.com.maxencemottard.swq'] },
+    };
   }
 }
