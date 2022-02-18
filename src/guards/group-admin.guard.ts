a529d67b97b0c01940140aa8d729b275714bec76
MaxenceMottard <maxence.mottard@worldline.com>
2022-02-18 15:55:08 +0100

    send activation mail when add admin to group

--- src/guards/group-admin.guard.ts
+++ src/guards/group-admin.guard.ts
@@ -14,6 +14,7 @@ export class GroupAdminGuard implements CanActivate {
 
     return !(
       !group.organization ||
+      // @ts-ignore
       (!group.organization.admins.includes(request.user._id) &&
         !request.user.isAdmin)
     );
