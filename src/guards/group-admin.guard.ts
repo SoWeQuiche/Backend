feb81756c5c73a7fddc782cb6e96d676149689d8
MaxenceMottard <maxence.mottard@worldline.com>
2022-02-18 15:59:26 +0100

    fixs

--- src/guards/group-admin.guard.ts
+++ src/guards/group-admin.guard.ts
@@ -14,7 +14,6 @@ export class GroupAdminGuard implements CanActivate {
 
     return !(
       !group.organization ||
-      // @ts-ignore
       (!group.organization.admins.includes(request.user._id) &&
         !request.user.isAdmin)
     );
