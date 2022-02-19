f5dd3a5e0c001c2a08de0c236208c92edec884e7
MaxenceMottard <maxence.mottard@worldline.com>
2022-02-19 17:52:14 +0100

    install packages

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import config from './config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,8 @@ async function bootstrap() {
     .build();
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, swaggerDocument);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(config.port);
 }
